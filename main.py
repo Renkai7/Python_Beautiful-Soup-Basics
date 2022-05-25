2a25bdd4a5925c91df28eb943c2a79bcf0ef4f6f
Mike <ms53150@gmail.com>
2022-05-24 20:54:52 -0400

    Read html tags and text using bs4

---
+++ main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+
+with open("website.html", encoding="utf-8") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, 'html.parser')
+# print(soup.title)
+# print(soup.prettify())
+
+print(soup.a)
