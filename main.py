ed1398cccaecf8b089fc06d230d436dbe9a33200
Mike <ms53150@gmail.com>
2022-05-25 21:18:39 -0400

    Use for loop to get all href texts

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ soup = BeautifulSoup(contents, 'html.parser')
 # Find all of an element
 all_anchor_tags = soup.find_all(name="a")
 print(all_anchor_tags)
+
+for tag in all_anchor_tags:
+    # print(tag.getText)
+    print(tag.get("href"))
