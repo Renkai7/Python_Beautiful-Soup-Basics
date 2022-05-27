6c0177fc6fe5d2ae8d8e2249e87ce192e3cc04f7
Mike <ms53150@gmail.com>
2022-05-26 21:31:10 -0400

    Use soup to find html element that contains a class

--- main.py
+++ main.py
@@ -16,4 +16,7 @@ all_anchor_tags = soup.find_all(name="a")
     # print(tag.get("href"))
 
 heading = soup.find(name="h1", id="name")
-print(heading)
+# print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading)
