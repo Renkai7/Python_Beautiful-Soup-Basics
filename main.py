b1d414d720c8e851450db42d53d16513fb8afba2
Mike <ms53150@gmail.com>
2022-05-28 22:25:33 -0400

    Scrap website for article title, link, and upvote text

--- main.py
+++ main.py
@@ -4,30 +4,40 @@ import requests
 with open("website.html", encoding="utf-8") as file:
     contents = file.read()
 
-soup = BeautifulSoup(contents, 'html.parser')
+first_soup = BeautifulSoup(contents, 'html.parser')
 # print(soup.title)
 # print(soup.prettify())
 
 # Find all of an element
-all_anchor_tags = soup.find_all(name="a")
+all_anchor_tags = first_soup.find_all(name="a")
 # print(all_anchor_tags)
 
 # for tag in all_anchor_tags:
     # print(tag.getText)
     # print(tag.get("href"))
 
-heading = soup.find(name="h1", id="name")
+heading = first_soup.find(name="h1", id="name")
 # print(heading)
 
-section_heading = soup.find(name="h3", class_="heading")
+section_heading = first_soup.find(name="h3", class_="heading")
 # print(section_heading)
 
-company_url = soup.select_one(selector="p a")
-name = soup.select_one(selector="#name")
+company_url = first_soup.select_one(selector="p a")
+name = first_soup.select_one(selector="#name")
 # print(name)
 #
-headings = soup.select(".heading")
+headings = first_soup.select(".heading")
 # print(headings)
 
 response = requests.get("https://news.ycombinator.com/news")
-print(response.text)
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+article_tag = soup.find(name="a", class_="titlelink")
+article_text = article_tag.getText()
+article_link = article_tag.get("href")
+article_upvote = soup.find(name="span", class_="score").getText()
+
+print(article_tag.text)
+print(article_link)
+print(article_upvote)
